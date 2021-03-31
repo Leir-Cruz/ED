e0afda7447c6a95bc5fcc6e91a27e1238a10f345
Gabriel Cruz <gabrielcruzvazsantos@gmail.com>
2021-03-31 16:44:56 -0300

    funcoes adicionar_alfabeto e decifrar concluidas

--- trabalho1/trabalho1.py
+++ trabalho1/trabalho1.py
@@ -26,6 +26,33 @@ class Deque:
             sdeque += i
         return sdeque
 
-
 d = Deque()
-print(d.is_empty())
+
+def adicionar_alfabeto(deque, alfabeto):
+    for l in alfabeto:
+        deque.add_front(l)
+
+def decifrar(deque, texto_cifrado, chave):
+    alfabeto, size_cifrado, decifrada = deque.__str__(), len(texto_cifrado), ''
+    for i in range(len(texto_cifrado)):
+        for j in range(len(alfabeto)):
+            if texto_cifrado[i] == alfabeto[j]:
+                if j - chave < 0:
+                    negative = j - chave
+                    new_letter = alfabeto[len(alfabeto) - (-negative)]
+                    removed = deque.remove_rear()
+                    deque.add_front(removed)
+                    decifrada += new_letter
+                    break
+                else:
+                    new_letter = alfabeto[j - chave]
+                    removed = deque.remove_rear()
+                    deque.add_front(removed)
+                    decifrada += new_letter
+                    break
+    return decifrada
+
+adicionar_alfabeto(d, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+print(decifrar(d, "DEFGHIJKLMNOPQRSTUVWXYZABC", 3))
+print(d.__str__())
+
